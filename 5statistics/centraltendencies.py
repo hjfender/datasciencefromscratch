83a8dfd5ce468f34536a56d2f0c8db8fd695b197
hjfender <hfender@itm21st.com>
2019-02-28 17:10:53 -0600

    Start statistics

---
+++ 5statistics/centraltendencies.py
@@ -0,0 +1,46 @@
+from describingdata import num_friends
+
+def mean(x):
+    return sum(x) / len(x)
+
+mean(num_friends)   # 7.333333...
+
+def median(v):
+    """finds the 'middle-most' value of v"""
+    n = len(v)
+    sorted_v = sorted(v)
+    midpoint = n // 2
+
+    if n % 2 == 1:
+        # if odd, return the middle value
+        return sorted_v[midpoint]
+    else:
+        # if even, return the average of the middle values
+        lo = midpoint - 1
+        hi = midpoint
+        return (sorted_v[lo] + sorted_v[hi]) / 2
+
+median(num_friends) # 6.0
+
+# generalization of median
+def quantile(x, p):
+    """returns the pth-percentile value in x"""
+    p_index = int(p * len(x))
+    return sorted(x)[p_index]
+
+# median is the 0.50 quantile, value that 50% of the date lies under
+
+quantile(num_friends, 0.10) # 1
+quantile(num_friends, 0.25) # 3
+quantile(num_friends, 0.75) # 9
+quantile(num_friends, 0.90) # 13
+
+# most common value
+def mode(x):
+    """returns a list, might be more than one mode"""
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.iteritems()
+            if count == max_count]
+
+mode(num_friends)   # 1 and 6
